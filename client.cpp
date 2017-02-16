c61c8ce25e4e0ad71c23f8678a1e0a2f79740e2a
pankaj0001 <pankajkukreja0001@gmail.com>
2017-02-16 16:31:53 +0530

    Client- InProcess , Server - Done

--- client.cpp
+++ client.cpp
@@ -8,28 +8,35 @@ int main()
 
 	sf::UdpSocket socket;
 	socket.bind(55001);
-	string server_ip = "172.16.4.28";
+	sf::IpAddress server_ip = sf::getRemoteAddress();//Change it to cin>>" Enter Serve IP - "<<endl;
 	unsigned short server_port = 55002;
-
-	
+	sf::Packet server_send_initital, server_send, server_recieve_initital, server_recieve;
+	sf::Uint16 temp, positionx, positiony, id, next_position_x = 0, next_position_y = 0;
 	
 	//Stor in packet
-	sf::Packet packet_server_recieve;
+	std::cout << "Do you want to Connect to server 1=Yes, ^1 =No ? -> ";
+	cin>>temp;
+ 	server_send_initital<<temp
+	socket.send(server_send_initital, server_ip, server_port);
 
-	//Send To Server
-	socket.send(packet, server_ip, server_port);
 
+	socket.receive(server_recieve_initital, sender, port);
+	server_recieve_initital>>id>>positionx>>positiony;
+	next_position_x = positionx++;
+	next_position_y = positiony++;
+	// " Initial Positions are positionx and positiony and myuserid = id "
 
-		
-	//Recieve From Server
-	char buffer[1024];
-	std::size_t received = 0;
-	sf::IpAddress sender;
-	unsigned short port;
-	socket.receive(packet, sender, port);
-	sf::Uint16 temp;
-	packet>>temp;
-	std::cout << sender.toString() << " said: " << temp << std::endl;
+	while(true)
+	{
+		server_send << id << positionx << positiony;
+		socket.send(server_send, server_ip, server_port);
+		server_sende.clear();
+		socket.receive(server_recieve, sender, port);
+		server_recieve >> id >> positionx >> positiony;
+		next_position_x = positionx++;
+		next_position_y = positiony++;
+
+	}
 	return 0;
 
 
--- server.cpp
+++ server.cpp
@@ -7,10 +7,18 @@ using namespace std;
 int main()
 {
 	cout << "Starting the server" << endl;
-	int , player_count = 0;
+	int i,player_count = 0;
+	sf::UdpSocket socket;
 	sf::IpAddress server_IP, Client_IP, Players_IP[4];
-	unsigned short Client_Port,Players_PORT[4];
+	unsigned short Client_Port,Players_PORT[4],port = 55002;
 	bool take=true;
+
+	if(socket.bind(port) != sf::Socket::Done)
+	{
+		std::cout << "Error in Port Binding port tried = "<< port << " " << std::endl;
+		return 0;
+	}
+	std::cout << "Using Port = "<< port << " For Incomming Connections" << std::endl;
 
 
 	//Defining Packets
@@ -20,9 +28,9 @@ int main()
 
 	//Waititng For All Players To Join
 	//Client: Hi Server! I am a player
-	while(take && player_count != 4)
+	while(take && player_count != 2)
 	{
-		cout<<" Waiting For Connections \n Press s to stop waiting \n -> ";
+		//cout<<" Waiting For Connections \n Press s to stop waiting \n -> ";
 		//Thread Here which check if input was there
 
 		//blocking Recieve so in thread
@@ -43,26 +51,23 @@ int main()
 	{
 		position_x[i] = (sf::Uint16)(rand()%184);
 		position_y[i] = (sf::Uint16)(rand()%105);
-		packet_client_initial_send << id << position_x[i] << position_y[i];
-
+		packet_client_initial_send << i << position_x[i] << position_y[i];
+		socket.send(packet_client_initial_send, Players_IP[i], Players_PORT[i]);
+		packet_client_initial_send.clear();
+		// packet_client_initial_send clear it before next Loop
 		//-------> wait for send complete or time >= wait_limit
-
-
-		if(socket.send(packet_client_initial_send, Players_IP[i], Players_PORT[i]))
-		{
-			//-------> Connection Lost To Players_IP[i] so clear this ip from list
-			//player_count--;
-			//i++;
-		}
-		//------------> packet_client_initial_send clear it before next Loop
 	}
+
+	sf::Time t1 = sf::seconds(0.1f);
+	sf::Clock clock;
 
 	while(true)
 	{
 		//Recieve Next Move of all( time wait 100 ms) (blocking Recieve so in thread)
-		while(time <= 100ms) //     <-------------------
+		sf::Time time1 = clock.getElapsedTime();
+		while(time1 <= t1)
 		{
-			socket.receive(packet_server_recieve, Client_IP, Client_Port)
+			socket.receive(packet_server_recieve, Client_IP, Client_Port);
 			packet_server_recieve>> id >> positionx >> positiony;
 			position_x[id] = positionx;
 			position_y[id] = positiony;
@@ -77,14 +82,13 @@ int main()
 		for(i=0;i<player_count;i++)
 		{
 			//check if sent
-			if(socket.send(packet_server_send_all, Players_IP[i], Players_PORT[i]))
-			{
-
-			}
+			//sf::IpAddress::Broadcast To BroadCast to all Ips
+			socket.send(packet_server_send_all, sf::IpAddress::Broadcast, Client_Port);
 		}
 
-		//--------> packet_server_send_all clear this packet
+		packet_server_send_all.clear(); //packet_server_send_all clear this packet
 
+	sf::Time time2 = clock.restart();
 	}
 	cout << endl;
 	return 0;
@@ -98,8 +102,6 @@ int main()
 	server_IP = sf::IpAddress::getLocalAddress();
 	cout << "Server Ip Address is "<< server_IP.toString() << endl;
 
-	sf::UdpSocket socket;
-	socket.bind(55002);
 
 
 	//Sender = IP of Client who sent data
